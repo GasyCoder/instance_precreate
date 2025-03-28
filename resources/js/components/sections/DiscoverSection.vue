d8bc5c6f070574a0a48d95f67340a12bfd3c6c7f
Francky <kyfr.rakotonirina1@gmail.com>
2025-03-28 14:45:11 +0100

    fixed

--- resources/js/components/sections/DiscoverSection.vue
+++ resources/js/components/sections/DiscoverSection.vue
@@ -1,5 +1,5 @@
 <template>
-    <section class="section-py bg-body">
+    <section id="landingHero" class="min-vh-100 d-flex align-items-center position-relative overflow-hidden bg-light">
       <div class="container bg-icon-left position-relative">
         <img src="/assets/img/front-pages/icons/bg-left-icon-light.png"
              alt="section icon"

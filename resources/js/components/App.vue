bb63de9f0b2c1e7d70879d3aa52538f0d043c3e7
Francky <kyfr.rakotonirina1@gmail.com>
2025-03-28 14:17:56 +0100

    add text

--- resources/js/components/App.vue
+++ resources/js/components/App.vue
@@ -3,6 +3,7 @@
       <nav-section />
       <div data-bs-spy="scroll" class="scrollspy-example">
         <hero-section />
+        <discover-section />
         <pricing-section />
         <facts-section />
         <faq-section />
@@ -25,12 +26,14 @@ import CTASection from './sections/CTASection.vue'
 // import ContactSection from './sections/ContactSection.vue'
 import FooterSection from './sections/FooterSection.vue'
 import ScrollSection from './sections/ScrollSection.vue'
+import DiscoverSection from './sections/DiscoverSection.vue'
 
 export default {
     name: 'App',
     components: {
         NavSection,
         HeroSection,
+        DiscoverSection,
         PricingSection,
         FactsSection,
         FaqSection,
--- resources/js/components/sections/HeroSection.vue
+++ resources/js/components/sections/HeroSection.vue
@@ -60,35 +60,6 @@
                 </div>
             </div>
         </div>
-
-        <div class="container bg-icon-left position-relative">
-        <img src="/assets/img/front-pages/icons/bg-left-icon-light.png"
-             alt="section icon"
-             class="position-absolute top-0 start-0"
-             data-speed="1" />
-
-        <!-- Header Original -->
-        <h5 class="text-center mb-2">
-          <span class="display-5 fs-4 fw-bold">Découvrez nos solutions sur mesure</span> 
-        </h5>
-        <p class="text-center fw-medium mb-10">
-          Quel que soit votre organisation, que vous soyez entrepreneur individuel, PME en pleine croissance ou grande entreprise, ou même association ou administration. ERP INNOV a la solution adaptée à vos besoins. Explorez notre gamme d'outils de gestion et notre catalogue de formations conçus pour propulser votre entreprise vers le succès.
-        </p>
-
-        <p class="text-center fw-medium mb-10">
-            Avec ERP INNOV, vous bénéficiez de :
-            <ul>
-                <li>Des outils de gestion puissants et intuitifs</li>
-                <li>Un accès à vos données partout et à tout moment</li>
-                <li>Des formations pratiques pour développer votre entreprise </li>
-            </ul>
-        </p>
-
-        <p class="text-center fw-medium mb-10">
-          Prêt à transformer votre gestion et booster votre croissance ? Parcourez nos offres et trouvez la solution idéale pour votre entreprise.
-        </p>
-
-      </div>
     </section>
 </template>
 

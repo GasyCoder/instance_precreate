c16d322ad2227dccc373fabbf17a0195d47bd33a
Francky <kyfr.rakotonirina1@gmail.com>
2025-03-28 13:23:24 +0100

    update icon

--- resources/js/components/sections/FactsSection.vue
+++ resources/js/components/sections/FactsSection.vue
@@ -1,7 +1,7 @@
 <template>
     <section id="landingFunFacts" class="section-py landing-fun-facts py-12 my-3">
         <div class="container">
-            <div class="row gx-0 gy-6 gx-sm-6">
+            <div class="row gx-0 gy-6 gx-sm-6 justify-content-center">
                 <div v-for="fact in facts"
                      :key="fact.id"
                      class="col-md-3 col-sm-6 text-center"

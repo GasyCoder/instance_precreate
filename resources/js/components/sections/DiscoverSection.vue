fc009687e367a9f0c8edb107f43d00c0bd802846
Francky <kyfr.rakotonirina1@gmail.com>
2025-03-30 05:47:19 +0200

    fixed

--- resources/js/components/sections/DiscoverSection.vue
+++ resources/js/components/sections/DiscoverSection.vue
@@ -8,29 +8,20 @@
 
         <!-- Header Original -->
         <h5 class="text-center mb-2">
-          <span class="display-5 fs-4 fw-bold">Découvrez nos solutions sur mesure</span> 
+          <span class="display-5 fs-4 fw-bold">Découvrez nos solutions sur mesure pour <span class="organisation">votre organisation</span></span> 
         </h5>
         <p class="text-center fw-medium mb-10">
-          Quel que soit votre organisation, que vous soyez entrepreneur individuel, PME en pleine croissance ou grande entreprise, ou même association ou administration. ERP INNOV a la solution adaptée à vos besoins. Explorez notre gamme d'outils de gestion et notre catalogue de formations conçus pour propulser votre entreprise vers le succès.
+          Quel que soit votre organisation, que vous soyez entrepreneur individuel, PME en pleine croissance ou grande entreprise, ou même association ou administration. 
+          ERP INNOV a la solution adaptée à vos besoins. Explorez notre gamme d'outils de gestion et notre catalogue de formations conçus pour propulser votre entreprise vers le succès.
         </p>
-
-        <p class="text-center fw-medium mb-10">
-            Avec ERP INNOV, vous bénéficiez de :
-            <ul>
-                <li>Des outils de gestion puissants et intuitifs</li>
-                <li>Un accès à vos données partout et à tout moment</li>
-                <li>Des formations pratiques pour développer votre entreprise </li>
-            </ul>
-        </p>
-
-        <p class="text-center fw-medium mb-10">
-          Prêt à transformer votre gestion et booster votre croissance ? Parcourez nos offres et trouvez la solution idéale pour votre entreprise.
-        </p>
-
       </div>
     </section>
   </template>
-
+<style>
+  .organisation {
+        color: #0393da;
+    }
+ </style>
 <script>
  export default {
     name: 'DiscoverSection'
